ed1efe729382a38d7f4ec8d83e955284aa0149d2
Hamza Bilal <hamzabilal770@gmail.com>
2023-02-07 04:24:14 -0800

    remaining

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,3 +1,13 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
+let start = document.getElementById('play');
+
+start.addEventListener('click', function(){
+  let i = document.getElementById('hide');
+  i.style.display = 'none'
+  
+  // let p = document.getElementById('hide');
+  // p.style.display = 'none'
+})
